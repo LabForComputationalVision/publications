d51ffefc377da382af60e299a35b08aa71f8aa0d
William F. Broderick <billbrod@gmail.com>
2025-09-05 15:08:10 -0400

    remove jekyll verbose

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ grep -axv '.*' bibliography_files/*txt && exit 1
 grep -axv '.*' bibliography_files/abstracts/*txt && exit 1
 
 python bibliography_files/prepare_bib.py bibliography_files/simoncelli.bib --aux bibliography_files/simoncelli.bibaux --abstracts bibliography_files/abstracts/ --author_url bibliography_files/authorURL.txt --journal_url bibliography_files/journalURL.txt
-bundle exec jekyll build --verbose -d ./_site/publications
+bundle exec jekyll build -d ./_site/publications
 sed -E -i.backup "s/(h2.*)'\{e\}/\1é/g" _site/publications/author.html
 sed -E -i.backup "s/(h2.*)\`\{e\}/\1è/g" _site/publications/author.html
 
